f9a75c1b9f57afb4aaa740d164e8c37c62aaa87f
unknown <iris.litiu@gmail.com>
2023-03-11 14:42:34 -0800

    Initial commit of drive teams mods to code <3 - drive team

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -75,7 +75,7 @@ public final class Constants {
     // The MAXSwerve module can be configured with one of three pinion gears: 12T, 13T, or 14T.
     // This changes the drive speed of the module (a pinion gear with more teeth will result in a
     // robot that drives faster).
-    public static final int kDrivingMotorPinionTeeth = 14;
+    public static final int kDrivingMotorPinionTeeth = 13;
 
     // Invert the turning encoder, since the output shaft rotates in the opposite direction of
     // the steering motor in the MAXSwerve Module.
@@ -128,8 +128,8 @@ public final class Constants {
     public static final double kDriveDeadband = 0.06;
     public static final double kMagnitudeDeadband = 0.06;
     public static final double kDirectionSlewRate = 1.2; // radians per second
-    public static final double kMagnitudeSlewRate = 1.8; // percent per second (1 = 100%)
-    public static final double kRotationalSlewRate = 2.0; // percent per second (1 = 100%)
+    public static final double kMagnitudeSlewRate = 4.0; // percent per second (1 = 100%)
+    public static final double kRotationalSlewRate = 4.0; // percent per second (1 = 100%)
 
     //button bindings
     public static final int INTAKE_SPEED_AXIS = 3;
