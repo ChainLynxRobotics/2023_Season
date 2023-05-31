461b821c0391396de0492f5a9c68f149ade8bdce
Irishumanoid <95321751+Irishumanoid@users.noreply.github.com>
2023-05-31 16:20:01 -0700

    direct feed profile and grapher for motion profile simulations prior to testing on our elevator

--- src/main/java/frc/Lib/MotionProfiles/IProfiler.java
+++ src/main/java/frc/Lib/MotionProfiles/IProfiler.java
@@ -1,7 +1,12 @@
 package frc.Lib.MotionProfiles;
 
 public interface IProfiler {
-    
+
+    public interface Config<T extends Config<T>> {
+
+        public boolean atConfig(T config);
+    }
+
     public double calculate(double t);
 
     public boolean setpointReached(double curPos, double setpoint, double error);
